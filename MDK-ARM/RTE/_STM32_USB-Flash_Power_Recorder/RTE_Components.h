f8e956bd1f41fc69ce9046c88c1ae4a2eb876e46
Alex <wjx.sir@qq.com>
2021-12-26 16:07:55 +0800

    Start designing strcuture
    USB AM(F) 5pin VOOC

---
+++ MDK-ARM/RTE/_STM32_USB-Flash_Power_Recorder/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32 USB-Flash Power Recorder' 
+ * Target:  'STM32 USB-Flash Power Recorder' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
